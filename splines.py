99fd336e7b5bbbfdb66235aa9263113955ae784f
ds12a <dshen1248@gmail.com>
2025-10-11 10:23:32 -0400

    spline interpolation

--- splines.py
+++ splines.py
@@ -1,21 +1,51 @@
 from stolen import Trajectory, CostMap, Context, cartesian_to_ij
 
-import matplotlib as plt
-import scipy
+import matplotlib.pyplot as plt
+from scipy.interpolate import make_splprep, splev
 import numpy as np
 
 import math, sys
 
+
 class SplineInterpolation:
 
-    @staticmethod
-    def generate_points_in_segment(ctx: Context, trajectory: Trajectory, cost_map: CostMap) -> np.ndarray:
-        pass
+    # @staticmethod
+    # def generate_points_in_segment(
+    #     ctx: Context, trajectory: Trajectory, cost_map: CostMap
+    # ) -> np.ndarray:
+    #     pass
 
     @staticmethod
-    def interpolate(ctx: Context, trajectory: Trajectory, cost_map: CostMap) -> Trajectory:
+    def interpolate(
+        ctx: Context, trajectory: Trajectory, cost_map: CostMap, spacing: float = 0.25
+    ) -> Trajectory:
+        """
+        Fits cubic splines to the given trajectory and returns a new trajectory with 
+        evenly spaced points sampled from the splines. We approximate the total distance
+        of the calculated splines and map the desired distances of each point to the 
+        default parameterization produce by scipy.
+        """
+        # can configure smoothness (s) and other parameters
+        spline, u = make_splprep(
+            [trajectory.coordinates[:, 0], trajectory.coordinates[:, 1]], s=0
+        )
 
-        # scipy interpolate
-        # loop through each segment, generate points
-        # bing bang boom return
-        pass
+        # make really smooth for accurate distance
+        u_fine = np.linspace(u.min(), u.max(), 5000)  # TODO find good number
+        x_fine, y_fine = splev(u_fine, spline)
+
+        # calculate distance from start for each point
+        dist = np.cumsum(np.sqrt(np.diff(x_fine) ** 2 + np.diff(y_fine) ** 2))
+        dist = np.insert(dist, 0, 0)
+
+        samples = int(dist[-1] / spacing)
+        target_dist = np.linspace(0, samples * spacing, samples + 1)
+
+        u_spaced = np.interp(target_dist, dist, u_fine)  # map distance to u parameter
+        u_spaced = np.append(u_spaced, u[-1])  # make sure end waypoint is preserved
+
+        # np.concatenate(u_spaced, u) TODO maybe also include original u in input so that all original waypoints are still used
+
+        x_spaced, y_spaced = splev(u_spaced, spline)
+
+        return Trajectory(np.column_stack([x_spaced, y_spaced]))
