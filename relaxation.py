645ba3b6919ccbc14932e8bc9781299b7f1f393f
QuickGiveMeMemes <quickgivemememes@gmail.com>
2025-10-16 18:29:57 -0400

    david

--- relaxation.py
+++ relaxation.py
@@ -26,7 +26,7 @@ class Relaxation:
             + np.pow(trajectory.coordinates[i][1] - trajectory.coordinates[j][1], 2)
         )
 
-        points_per_m = 4
+        points_per_m = 2
         interpolate = int(points_per_m * dist)  # total number of interpolated points
         segment_interval = dist / interpolate  # distance between interpolated points
         # print(segment_interval)
@@ -35,13 +35,13 @@ class Relaxation:
             (
                 np.linspace(
                     trajectory.coordinates[i][0],
-                    trajectory.coordinates[j][0],
+                    trajectory.coordinates[j][0] + 0.1,
                     interpolate,
                     endpoint=False
                 ),
                 np.linspace(
                     trajectory.coordinates[i][1],
-                    trajectory.coordinates[j][1],
+                    trajectory.coordinates[j][1] + 0.1,
                     interpolate,
                     endpoint=False
                 ),
@@ -58,12 +58,12 @@ class Relaxation:
                 segment_interval * cost_map.data[*cartesian_to_ij(ctx, np.array([x, y]))[::-1]]
             )  # TODO check if this cost function can "overcount" cost
 
-            # foo = segment_interval * cost_map.data[*cartesian_to_ij(ctx, np.array([x, y]))[::-1]]
-            # if i == 0 and j == 2 and foo != 0:
-            #     print("DEBUG")
-            #     print(trajectory)
-            #     print(x, y, foo)
-            #     print("\n\n")
+            foo = segment_interval * cost_map.data[*cartesian_to_ij(ctx, np.array([x, y]))[::-1]]
+            if foo != 0:
+                print("DEBUG")
+                print(trajectory)
+                print(x, y, foo)
+                print("\n\n")
         return cost
 
     @staticmethod
